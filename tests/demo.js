2811701927f96790bf74734ab859058c6e9f9944
mdsulemanr <mdsulemanr@gmail.com>
2026-02-12 13:31:59 +0500

    javascript vs typescript

---
+++ tests/demo.js
@@ -0,0 +1,25 @@
+let name = 'Hello World';
+console.log(name);
+
+let age = 30;
+console.log(age);
+
+let isStudent = true;
+console.log(isStudent);
+
+let hobbies = ['reading', 'coding', 'traveling'];
+console.log(hobbies);
+
+let person = {
+    name: 'John Doe',
+    age: 25,
+    isStudent: false
+};
+console.log(person);
+
+function add(a, b) {
+    return a + b;
+};
+add(5, 10);
+
+
